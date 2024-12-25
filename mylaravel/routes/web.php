97f5fa7da69c1a8970b46aeac39b7a9e47ff0e1a
thanaporn-pho <thanaporn.phoa@gmail.com>
2024-12-25 18:06:05 +0700

    create laravel

---
+++ mylaravel/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return "<h1>Hello World!</h1>";
+});
